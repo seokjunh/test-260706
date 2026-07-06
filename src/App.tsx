8cff9502d5f763f9d597ba659622a6f77ba5046d
seokjunh <seokjunh97@gmail.com>
2026-07-06 13:54:07 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
